900ce45e6cc4286b06822bdc3f46501fc18a7658
Tofazzal Hossain <mdtofazzalislam6789@gmail.com>
2025-02-21 00:22:29 +0600

    product cart and wishlist style finish

--- src/app/_Components/MainContent/page.jsx
+++ src/app/_Components/MainContent/page.jsx
@@ -6,7 +6,7 @@ import Sidebar from "../Sidebar/page";
 const MainContent = () => {
   return (
     <div className="mainContainer">
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-5">
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-5">
         <div className="col-span-1">
           <Sidebar />
         </div>
--- src/app/_Components/ProductContent/Card/page.jsx
+++ src/app/_Components/ProductContent/Card/page.jsx
@@ -1,44 +1,58 @@
 "use client";
-import { ShoppingCart } from "lucide-react";
+import { Eye, Heart, ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 import { useState } from "react";
 import { Star } from "@smastrom/react-rating";
+import CardStyle from "./CardStyle/CardStyle.module.css";
 
 const Card = () => {
   const [rating, setRating] = useState(0);
 
   return (
-    <div>
-      <div>
-        <div>
-          <Image
-            src="/product13.png"
-            width={200}
-            height={200}
-            alt="productImage"
-          />
-        </div>
-        <div>
+    <div className="mt-5">
+      <div className="lg:grid-cols-4 grid md:grid-cols-3 grid-cols-1">
+        <div className={`border-2 rounded-lg p-6 relative ${CardStyle.card}`}>
           <div>
-            <h4 className="text-base text-gray-600">Big Potatos</h4>
-            <h2 className="text-lg">$14.99</h2>
-            <div className="flex">
-              <Rating
-                style={{ maxWidth: 120 }}
-                value={4}
-                readOnly
-                itemStyles={{
-                  itemShapes: Star,
-                  activeFillColor: "#fe8900",
-                  inactiveFillColor: "#dcdcdc",
-                }}
-              />
+            <Image
+              src="/product13.png"
+              width={200}
+              height={200}
+              alt="productImage"
+              className={`w-full ${CardStyle.cardImage} `}
+            />
+          </div>
+          <div className="flex justify-between items-center mt-14">
+            <div>
+              <h4 className="text-base text-gray-600">Big Potatos</h4>
+              <h2 className="text-lg">$14.99</h2>
+              <div className="flex">
+                <Rating
+                  style={{ maxWidth: 80 }}
+                  value={4}
+                  readOnly
+                  itemStyles={{
+                    itemShapes: Star,
+                    activeFillColor: "#fe8900",
+                    inactiveFillColor: "#dcdcdc",
+                  }}
+                />
+              </div>
+            </div>
+            <div
+              className={`w-[45px] h-[45px] flex justify-center items-center rounded-full bg-gray-100 ${CardStyle.activeCart}`}
+            >
+              <ShoppingCart size={20} />
             </div>
           </div>
-          <div>
-            <ShoppingCart size={20} />
+          <div className={`space-y-2 ${CardStyle.overlay}`}>
+            <div className="w-[45px] h-[45px] flex justify-center items-center rounded-full bg-white border-2">
+              <Heart size={20} />
+            </div>
+            <div className="w-[45px] h-[45px] flex justify-center items-center rounded-full bg-white border-2">
+              <Eye size={20} />
+            </div>
           </div>
         </div>
       </div>
